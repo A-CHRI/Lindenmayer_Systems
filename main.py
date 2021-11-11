a286a7b88b8025603baa7f518c5a306e986e9f59
Xydros <alexander.anker.chr@gmail.com>
2021-11-11 20:58:32 +0100

    Provided some structure to the code

---
+++ main.py
@@ -0,0 +1,48 @@
+import turtle
+
+### Lindenmayer Iteration
+def LindIter(System, N):
+    pass
+    # Code here - remove the "pass" when you start coding
+    return LindenmayerString
+
+### Translation to turtle graphics commands
+def turtleGraph(LindenmayerString):
+    pass
+    # Code here
+    return turtleCommands
+
+### Turtle graphics plot function
+def turtlePlot(turtleCommands):
+    pass
+    # Code here - remove the "pass" when you start coding
+
+### Main script
+if __name__ == "__main__":
+    # List of options in the main menu
+    options = ["Choose Lindenmayer system", "Generate plots", "Quit"]
+    #While loop to make sure the user returns to the menu of the interface
+    while True:
+        # Creates a seperator for the layout
+        print("\n---------------------------------------")
+        # Prints the options in the menu, and gets the users input
+        print("\nPlease choose one of the following options by entering its corresponding number:\n")
+        for i,v in enumerate(options):
+            print(i+1, ":", v)
+        inp = input("\nInput: ")
+
+        # Runs if the user wishes to choose a system
+        if inp == "1":
+            pass # Remove the "pass" when you start coding
+
+        # Runs if the user wishes to generate plots
+        elif inp == "2":
+            pass # Remove the "pass" when you start coding
+
+        # Runs if the user wishes to quit
+        elif inp == "3":
+            break
+
+        # Runs if the user inputs an invalid option
+        else:
+            print("\nInvalid input. Please try again.")
