0aa7fb9968c71d6489ac00815f9931d8a73634b0
Alexander Anker Christensen <alexander.Anker.chr@gmail.com>
2021-12-01 18:46:32 +0100

    Commented the code

--- lindenmeyer.py
+++ lindenmeyer.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 
 ### Lindenmayer Iteration
 def LindIter(System, N):
+    # Initialize the Lindenmayer string depending on the choosing system
     if System == "Koch":
         LindenmayerString = "S"
         for i in range(N):
@@ -34,7 +35,10 @@ def LindIter(System, N):
 
 ### Translation to turtle graphics commands
 def turtleGraph(LindenmayerString, N):
+    # Initializes an array of turtle commands
     turtleCommands = np.array([])
+    # Translates the Lindenmayer string to turtle commands and appends them to the turtleCommands array
+    # If-statement to see which system is being used
     if "S" in LindenmayerString:
         length = (1/3)**N
         for e in LindenmayerString:
@@ -57,8 +61,10 @@ def turtleGraph(LindenmayerString, N):
 
 ### Turtle graphics plot function
 def turtlePlot(turtleCommands):
+    # Initializes the list of points to plot, and sets the forst point to be (0,0)
     pointlist = np.array([[0,0]])
     d = np.array([1,0])
+    # Iterates through the turtleCommands array and adds the points to the list of points
     for i, e in enumerate(turtleCommands):
         if i % 2 == 0:
             xn = np.array([pointlist[-1] + e * d])
@@ -66,6 +72,7 @@ def turtlePlot(turtleCommands):
         else:
             dn = np.dot(np.array([[math.cos(e), -math.sin(e)], [math.sin(e), math.cos(e)]]), d)
             d = dn
+    # Sets up, and plots the points
     fig, ax = plt.subplots()
     ax.plot(pointlist[:,0], pointlist[:, 1])
     string = str(syslist[system-1]) + " system with " + str(iterations) + " iterations"
@@ -74,19 +81,19 @@ def turtlePlot(turtleCommands):
 
 ### Main script
 if __name__ == "__main__":
-    # List of options in the main menu
+    # List of options in the main menu, and a variable for the choosen option.
     options = ["Choose Lindenmayer system", "Generate plots", "Quit"]
     inp=0
     commands = np.array([])
+    # List of systems, variables for the choosen system, and the number of iterations
     syslist = ["Koch", "Sierpinski"]
     system = 0
     iterations = 0
     #While loop to make sure the user returns to the menu of the interface
     while True:
-        # Creates a seperator for the layout
-        
         if(inp==0):
             if system == 1 or system == 2:
+                # Creates a seperator for the layout
                 print("\n---------------------------------------")
                 print("Current system is", syslist[system-1], "with", iterations, "iterations.")
             print("\n---------------------------------------")
@@ -94,6 +101,7 @@ if __name__ == "__main__":
             print("\nPlease choose one of the following options by entering its corresponding number:\n")
             for i,v in enumerate(options):
                 print(i+1, ":", v)
+            # Gets the users input
             inp = input("\nInput: ")
 
         # Runs if the user wishes to choose a system
@@ -101,15 +109,19 @@ if __name__ == "__main__":
             print("Which system would you like?\n")
             for j,l in enumerate(["Koch curve", "Sierpinski triangle","Return to menu"]):
                 print(j+1, ":", l)
+            # Gets the users input
             sys = input("\nInput: ")
 
             if sys=="3":
                 inp=0
                 continue
+
+            # Catches any errors in the input
             elif sys != "1" and sys != "2":
                 print("Invalid input. Try again.\n")
                 continue
 
+            # Gets the number of iterations and checks for any wrongful inputs
             n = input("\nHow many iterations do you want of your system?\n")
             try:
                 n = int(n)
@@ -120,10 +132,14 @@ if __name__ == "__main__":
                 print("Invalid input. Try again.\n")
                 continue
 
+            # Sets the system variable to the users input, and intitializes the commands.
             system = int(sys)
             commands = turtleGraph(LindIter(syslist[int(sys)-1], n), n)
             inp = 0
+
+        # Runs if the user wishes to generate plots
         elif inp == "2":
+            # Checks if the system variable is valid
             if system != 0:
                 turtlePlot(commands)
             else:
